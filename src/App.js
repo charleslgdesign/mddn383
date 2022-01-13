df864a2ec5f212b509caf7fa22bf2f5964066d1a
Charlie64N7 <79482179+Charlie64N7@users.noreply.github.com>
2022-01-13 15:23:04 +1300

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+ import React, { Component } from 'react'
+ import Drinks from './components/Drinks.js'
+ import ReturnBottle from './components/ReturnBottle.js'
+ import Products from './components/Products.js'
+ import Product from './components/Product.js'
+ import Returns from './components/Returns.js'
+ import Gallery from './components/Gallery.js'
+ import WhitePaper from './components/WhitePaper.js'
+ import Header from './components/Header.js'
+ import './App.css'
+ import Wallet from './components/Wallet.js'
+ import Interactions from'./components/Interactions.js'
+ import ReactDOM from 'react-dom'
+ import { ReactComponent as Logo} from './components/img/logo.svg'
+ import GalleryHeader from './components/GalleryHeader.js'
+ import WhitePaperHeader from './components/WhitePaperHeader.js'
+ import GetStartedHeader from './components/GetStartedHeader.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    class App extends Component { 
+
+render() {
+     return (
+
+            
+                
+                <div className="container">
+                  <div className="header-container">
+                  <Header /> 
+                  <WhitePaperHeader />
+                  <GalleryHeader />
+                  <GetStartedHeader />
+                  <Drinks />
+                  <Wallet />
+                    <Returns />
+                     
+                    
+                    
+                    
+                {/* <WhitePaper /> */}
+                </div>
+            </div>
+
+        
+     )
+ }
 }
-
-export default App;
+ 
+ export default App;
+  
